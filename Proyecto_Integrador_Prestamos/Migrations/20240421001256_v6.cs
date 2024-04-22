96f6abc6328068fd06c4e09018f2ef90e7a3af5b
jmvillafanamdoza <144737939+jmvillafanamdoza@users.noreply.github.com>
2024-04-21 22:04:35 -0500

    Apply new .gitignore rules

---
+++ Proyecto_Integrador_Prestamos/Migrations/20240421001256_v6.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Integrador_Prestamos.Migrations
+{
+    public partial class v6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prestamo",
+                columns: table => new
+                {
+                    NroPrestamo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Importe = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Sede = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Moneda = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cuotas = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdPrestatario = table.Column<int>(type: "int", nullable: false),
+                    IdPrestamista = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamo", x => x.NroPrestamo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestatario",
+                columns: table => new
+                {
+                    IdPrestatario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestatario", x => x.IdPrestatario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dni = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prestamo");
+
+            migrationBuilder.DropTable(
+                name: "Prestatario");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
